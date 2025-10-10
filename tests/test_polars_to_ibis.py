085e4685d8c9416ad4fc9723c74ee3693e8a4bfe
Chuck McCallum <mccallucc@gmail.com>
2025-10-10 09:18:05 -0400

    head() works the same for both

--- tests/test_polars_to_ibis.py
+++ tests/test_polars_to_ibis.py
@@ -3,6 +3,22 @@ import polars as pl
 
 from polars_to_ibis import polars_expr_to_ibis
 
+ibis.set_backend("polars")
+
+data = {
+    "ints": [1, 2, 3, 4],
+    "floats": [0.1, 0.2, 0.3, 0.4],
+    "strings": ["a", "b", "c", "d"],
+    "bools": [True, True, False, False],
+}
+
+polars_df = pl.DataFrame(data)
+ibis_table = ibis.memtable(polars_df)
+
+
+def test_head():
+    assert polars_df.head(1).to_dicts() == ibis_table.head(1).to_polars().to_dicts()
+
 
 def test_polars_expr_to_ibis():
     pl_expr = pl.col("foo").sum().over("bar")
