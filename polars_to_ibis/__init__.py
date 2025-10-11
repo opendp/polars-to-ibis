a39bfc2492821705846738dcf06b6b169e0f67f6
Chuck McCallum <mccallucc@gmail.com>
2025-10-11 15:20:02 -0400

    remove commented out code

--- polars_to_ibis/__init__.py
+++ polars_to_ibis/__init__.py
@@ -1,5 +1,6 @@
 """Convert Polars expressions to Ibis expressions"""
 
+import json
 from pathlib import Path
 
 import ibis
@@ -9,23 +10,19 @@ __version__ = (Path(__file__).parent / "VERSION").read_text().strip()
 
 
 def polars_to_ibis(lf: pl.LazyFrame, table_name: str) -> ibis.Table:
-    # TODO:
-    # - Serialize LazyFrame
-    # - Read schema
-    # - Translate expr into Ibis
-
-    # pl_json = lf.serialize(format='json')
-    # import json
-    # pl_dict = json.loads(pl_json)
-    # del pl_dict['Slice']["input"]["DataFrameScan"]["df"]
-    # pl_json_clean = json.dumps(pl_dict)
+    polars_json = lf.serialize(format="json")
+    polars_plan = json.loads(polars_json)
 
     polars_schema = lf.collect_schema()
     ibis_schema = _polars_schema_to_ibis(polars_schema)
+    ibis_table = ibis.table(ibis_schema, name=table_name)
 
-    # TODO: Make this more general!
-    ibis_table = ibis.table(ibis_schema, name=table_name).head(1)
-    return ibis_table
+    return _apply_polars_plan_to_ibis_table(polars_plan, ibis_table)
+
+
+def _apply_polars_plan_to_ibis_table(polars_plan: dict, table: ibis.Table):
+    # TODO: Use polars_plan!
+    return table.head(1)
 
 
 _type_map = {
@@ -39,20 +36,3 @@ _type_map = {
 
 def _polars_schema_to_ibis(schema: pl.Schema):
     return {k: _type_map[v] for k, v in schema.items()}
-
-
-# def polars_expr_to_ibis(pl_expr: pl.Expr) -> ibis.common.deferred.Deferred:
-#     # JSON serialization is deprecated,
-#     # but use it to start from something.
-#     # Down the road maybe we convert the binary to JSON?
-#     pl_expr_json = pl_expr.meta.serialize(format="json")
-#     pl_expr_dict = json.loads(pl_expr_json)
-#     return _polars_dict_to_ibis(pl_expr_dict)
-
-
-# def _polars_dict_to_ibis(pl_expr_dict: dict) -> ibis.common.deferred.Deferred:
-#     # TODO! Transform pl_expr_dict into the AST for Ibis
-#     node = ast.parse('ibis._.group_by("bar").agg(foo=ibis._.foo.sum())', mode="eval")
-
-#     py_expr = ast.unparse(node)
-#     return eval(py_expr, {"ibis": ibis})
