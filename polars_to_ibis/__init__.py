4fab2f0718d34df701cf9f1c450d401a2865e5ca
Chuck McCallum <mccallucc@gmail.com>
2025-10-14 08:58:15 -0400

    use built-in to convert schema

--- polars_to_ibis/__init__.py
+++ polars_to_ibis/__init__.py
@@ -14,7 +14,7 @@ def polars_to_ibis(lf: pl.LazyFrame, table_name: str) -> ibis.Table:
     polars_plan = json.loads(polars_json)
 
     polars_schema = lf.collect_schema()
-    ibis_schema = _polars_schema_to_ibis(polars_schema)
+    ibis_schema = ibis.expr.schema.Schema.from_polars(polars_schema)
     ibis_table = ibis.table(ibis_schema, name=table_name)
 
     return _apply_polars_plan_to_ibis_table(polars_plan, ibis_table)
@@ -30,17 +30,3 @@ def _apply_polars_plan_to_ibis_table(polars_plan: dict, table: ibis.Table):
         slice_params = polars_plan["Slice"]
         return table.limit(slice_params["len"], offset=slice_params["offset"])
     raise UnhandledPolarsException("Unhandled polars plan")
-
-
-_type_map = {
-    # TODO: Expand this!
-    pl.Int64: "int64",
-    pl.UInt32: "uint32",
-    pl.Float64: "float64",
-    pl.String: "string",
-    pl.Boolean: "boolean",
-}
-
-
-def _polars_schema_to_ibis(schema: pl.Schema):
-    return {k: _type_map[v] for k, v in schema.items()}
