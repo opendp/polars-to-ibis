22f0270b324455dc3f68e991a0959848fa1dda79
Chuck McCallum <mccallucc@gmail.com>
2025-11-06 14:47:06 -0500

    revert start to run

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 # PostgreSQL:
 brew install postgresql
-# Configure PostgreSQL to start on boot:
-brew services start postgresql
+brew services run postgresql
 # Tests will create and drop "default_table" in this database:
 createdb $USER
