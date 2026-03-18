0d96cde38de9830379bccf6082bf88c6a0ae88fe
Chuck McCallum <mccallucc@gmail.com>
2026-03-18 15:57:11 -0400

    debug: where did it fail?

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-WAIT=20
+RETRIES=20
 
 # PostgreSQL:
 PG='postgresql@16'
@@ -10,8 +10,9 @@ brew install $PG
 brew services start $PG
 PG_PRE=$( brew --prefix $PG )/bin
 
-for i in {1..$WAIT}
+for i in {1..$RETRIES}
 do
+  echo "Create postgres user..."
   # Tests will create and drop "default_table" in this database:
   $PG_PRE/createdb $USER && break || echo 'Try again...'
   sleep 1
@@ -24,11 +25,14 @@ brew install $MY
 brew services start $MY
 MY_PRE=$( brew --prefix $MY )/bin
 
-for i in {1..$WAIT}
+for i in {1..$RETRIES}
 do
+  echo "Create mysql user..."
   $MY_PRE/mysql -u root -e "CREATE USER $USER" && break || echo 'Try again...'
   sleep 1
 done
 # Tests will create and drop "default_table" in this database:
+echo "Create database..."
 $MY_PRE/mysql -u root -e "CREATE DATABASE $USER"
+echo "Grant privs..."
 $MY_PRE/mysql -u root -e "GRANT ALL ON $USER.* TO '$USER'@'%'"
