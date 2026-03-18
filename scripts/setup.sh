521d7f0e5bca11b9dd9ff553f9a871de74f05b9b
Chuck McCallum <mccallucc@gmail.com>
2026-03-18 15:40:30 -0400

    add prefixes for DB commands

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,28 +5,30 @@ set -euo pipefail
 WAIT=20
 
 # PostgreSQL:
-brew install postgresql@16
-brew services start postgresql@16
-[[ -n $CI ]] && echo 'export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"' >> /Users/runner/.bash_profile
+PG='postgresql@16'
+brew install $PG
+brew services start $PG
+PG_PRE=$( brew --prefix $PG )/bin
 
-for i in {1..10}
+for i in {1..$WAIT}
 do
   # Tests will create and drop "default_table" in this database:
-  createdb $USER && break || echo 'Try again...'
+  $PG_PRE/createdb $USER && break || echo 'Try again...'
   sleep 1
 done
 
 
 # MySQL:
-brew install mysql@8.4
-brew services start mysql@8.4
-[[ -n $CI ]] && echo 'export PATH="/opt/homebrew/opt/mysql@8.4/bin:$PATH"' >> /Users/runner/.bash_profile
+MY='mysql@8.4'
+brew install $MY
+brew services start $MY
+MY_PRE=$( brew --prefix $MY )/bin
 
-for i in {1..10}
+for i in {1..$WAIT}
 do
-  mysql -u root -e "CREATE USER $USER" && break || echo 'Try again...'
+  $MY_PRE/mysql -u root -e "CREATE USER $USER" && break || echo 'Try again...'
   sleep 1
 done
 # Tests will create and drop "default_table" in this database:
-mysql -u root -e "CREATE DATABASE $USER"
-mysql -u root -e "GRANT ALL ON $USER.* TO '$USER'@'%'"
+$MY_PRE/mysql -u root -e "CREATE DATABASE $USER"
+$MY_PRE/mysql -u root -e "GRANT ALL ON $USER.* TO '$USER'@'%'"
