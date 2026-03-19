d8108581ba4b3b72fbd8124751c21d5a68bbed4e
Chuck McCallum <mccallucc@gmail.com>
2026-03-19 14:32:32 -0400

    Copy serialization code; will not work w/o jsonschema

--- polars_to_ibis/__init__.py
+++ polars_to_ibis/__init__.py
@@ -1,6 +1,5 @@
 """Convert Polars expressions to Ibis expressions"""
 
-import json
 import re
 from pathlib import Path
 from typing import Any
@@ -43,11 +42,13 @@ def polars_to_ibis(lf: pl.LazyFrame, table_name: str) -> ibis.Table:
 
     # NOTE: Tests fail if the order of serialize() and collect_schema() is switched.
     # TODO: Understand whether the schema or the plan is changing.
-    polars_plan = json.loads(lf.serialize(format="json"))
+    from polars_to_ibis.serialization import Serialization
+
+    polars_plan = Serialization(lf)
     polars_schema = lf.collect_schema()
 
     ibis_schema = ibis.expr.schema.Schema.from_polars(polars_schema)
-    ibis_table = ibis.table(ibis_schema, name=table_name)
+    ibis_table = ibis.table(ibis_schema, name=table_name)  # type: ignore
 
     return _apply_polars_plan_to_ibis_table(polars_plan, ibis_table)
 
@@ -72,22 +73,17 @@ class UnhandledPolarsException(Exception):
     pass
 
 
-def _apply_polars_plan_to_ibis_table(polars_plan: dict[str, Any], table: ibis.Table):
-    polars_plan_keys = list(polars_plan.keys())
-    if len(polars_plan_keys) != 1:
-        raise UnexpectedPolarsException(  # pragma: no cover
-            f"Expected only a single key, not: {polars_plan_keys}"
-        )
-    operation = polars_plan_keys[0]
-    params = polars_plan[operation]
+def _apply_polars_plan_to_ibis_table(polars_plan, table: ibis.Table):  # type: ignore
+    operation = list(polars_plan.keys())[0]  # type: ignore
+    params = list(polars_plan.values())[0]  # type: ignore
 
     if operation == "DataFrameScan":
         return table
 
-    input_polars_plan = params.pop("input")
-    input_table = _apply_polars_plan_to_ibis_table(input_polars_plan, table)
+    input_polars_plan = params.pop("input")  # type: ignore
+    input_table = _apply_polars_plan_to_ibis_table(input_polars_plan, table)  # type: ignore
 
-    return _apply_operation_params_to_ibis_table(operation, params, input_table)
+    return _apply_operation_params_to_ibis_table(operation, params, input_table)  # type: ignore
 
 
 def _apply_operation_params_to_ibis_table(
---
+++ polars_to_ibis/serialization.py
@@ -0,0 +1,41 @@
+"""
+LazyFrame.serialize() does not return stable results between Polars versions,
+and may be dropped in the future.
+Pulling out the serialization and validation logic keeps the rest of the code simple.
+"""
+
+import json
+
+import jsonschema
+import polars as pl
+
+
+class UnexpectedPolarsException(Exception):
+    """
+    JSON structure is not what we expected.
+    """
+
+    pass
+
+
+class Serialization:
+    def __init__(self, lf: pl.LazyFrame):
+        self._serial = json.loads(lf.serialize(format="json"))
+        self._validate()
+
+    def _validate(self):
+        if len(self._serial.keys()) != 1:
+            raise UnexpectedPolarsException(  # pragma: no cover
+                f"Expected only a single key, not: {self.keys()}"
+            )
+
+        jsonschema.validate(self._serial, {"type": "object"})  # type: ignore
+
+    # def __getitem__(self, name: str):
+    #     return self._serial[name]
+
+    def keys(self):  # type: ignore
+        return self._serial.keys()  # type: ignore
+
+    def values(self):  # type: ignore
+        return self._serial.values()  # type: ignore
