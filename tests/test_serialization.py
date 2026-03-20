9d9f23b28280ca0877f59f04fc4e6140a1cb41f8
Chuck McCallum <mccalluc@users.noreply.github.com>
2026-03-20 15:47:12 -0400

    Parameterized serialization tests (#39)
    * test parameterization
    
    * add serialization test of count: expecting some failures in CI
    
    * remove postgres and mysql deps
    
    * remove manual flit install
    
    * generalize the replacer
    
    * Upgrade count params
    
    * tidy
    
    * more coverage skips

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,45 +1,62 @@
 from typing import Any
 
 import polars as pl
+import pytest
 
-from polars_to_ibis.serialization import Serialization
+from polars_to_ibis.serialization import Serialization, _replace  # type: ignore
+
+io_pairs = [
+    (
+        pl.LazyFrame().count(),
+        {
+            "Select": {
+                "expr": [
+                    {
+                        "Agg": {
+                            "Count": {  # In Polars 1.32, this is a list.
+                                "input": {"Selector": "Wildcard"},
+                                "include_nulls": False,
+                            }
+                        }
+                    }
+                ],
+                "input": {"DataFrameScan": "..."},
+                "options": {
+                    "run_parallel": True,
+                    "duplicate_check": True,
+                    "should_broadcast": True,
+                },
+            }
+        },
+    ),
+    (
+        pl.LazyFrame().sort(by="ints").head(1),
+        {
+            "Slice": {
+                "input": {
+                    "Sort": {
+                        "input": {"DataFrameScan": "..."},
+                        "by_column": [{"Column": "ints"}],
+                        "slice": None,
+                        "sort_options": {
+                            "descending": [False],
+                            "nulls_last": [False],
+                            "multithreaded": True,
+                            "maintain_order": False,
+                            "limit": None,
+                        },
+                    }
+                },
+                "offset": 0,
+                "len": 1,
+            }
+        },
+    ),
+]
 
 
-def replace(source: dict[str, Any], key: str, new_value: str) -> None:
-    """
-    >>> source = {"foo": {"bar": 42}}
-    >>> replace(source, "bar", "stub")
-    >>> source
-    {'foo': {'bar': 'stub'}}
-    """
-    for k in source.keys():
-        if k == key:
-            source[k] = new_value
-        elif isinstance(source[k], dict):
-            replace(source[k], key, new_value)
-
-
-def test_serialization():
-    lf = pl.LazyFrame().sort(by="ints").head(1)
+@pytest.mark.parametrize("lf,expected", io_pairs)
+def test_serialization(lf: pl.LazyFrame, expected: dict[str, Any]):
     serial = Serialization(lf)._serial  # type: ignore
-    replace(serial, "DataFrameScan", "...")
-    assert serial == {
-        "Slice": {
-            "input": {
-                "Sort": {
-                    "input": {"DataFrameScan": "..."},
-                    "by_column": [{"Column": "ints"}],
-                    "slice": None,
-                    "sort_options": {
-                        "descending": [False],
-                        "nulls_last": [False],
-                        "multithreaded": True,
-                        "maintain_order": False,
-                        "limit": None,
-                    },
-                }
-            },
-            "offset": 0,
-            "len": 1,
-        }
-    }
+    _replace(serial, "DataFrameScan", lambda _: "...")
+    assert serial == expected
