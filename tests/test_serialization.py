d61fdfd93b932f63467332aacd97786605fd5795
Chuck McCallum <mccallucc@gmail.com>
2026-03-20 13:27:14 -0400

    test parameterization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import polars as pl
+import pytest
 
 from polars_to_ibis.serialization import Serialization
 
@@ -19,27 +20,35 @@ def replace(source: dict[str, Any], key: str, new_value: str) -> None:
             replace(source[k], key, new_value)
 
 
-def test_serialization():
-    lf = pl.LazyFrame().sort(by="ints").head(1)
+io_pairs = [
+    (
+        pl.LazyFrame().sort(by="ints").head(1),
+        {
+            "Slice": {
+                "input": {
+                    "Sort": {
+                        "input": {"DataFrameScan": "..."},
+                        "by_column": [{"Column": "ints"}],
+                        "slice": None,
+                        "sort_options": {
+                            "descending": [False],
+                            "nulls_last": [False],
+                            "multithreaded": True,
+                            "maintain_order": False,
+                            "limit": None,
+                        },
+                    }
+                },
+                "offset": 0,
+                "len": 1,
+            }
+        },
+    )
+]
+
+
+@pytest.mark.parametrize("lf,expected", io_pairs)
+def test_serialization(lf: pl.LazyFrame, expected: dict[str, Any]):
     serial = Serialization(lf)._serial  # type: ignore
     replace(serial, "DataFrameScan", "...")
-    assert serial == {
-        "Slice": {
-            "input": {
-                "Sort": {
-                    "input": {"DataFrameScan": "..."},
-                    "by_column": [{"Column": "ints"}],
-                    "slice": None,
-                    "sort_options": {
-                        "descending": [False],
-                        "nulls_last": [False],
-                        "multithreaded": True,
-                        "maintain_order": False,
-                        "limit": None,
-                    },
-                }
-            },
-            "offset": 0,
-            "len": 1,
-        }
-    }
+    assert serial == expected
