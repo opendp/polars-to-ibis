e1a2b455049ec2144c2c5c5c76c593f7f5dfbf50
Chuck McCallum <mccalluc@users.noreply.github.com>
2026-04-02 09:09:34 -0400

    start registry decorators (#49)
    - Towards #46

---
+++ polars_to_ibis/_parser.py
@@ -0,0 +1,129 @@
+import re
+from typing import Any, Callable
+
+import ibis  # pyright: ignore [reportMissingTypeStubs]
+import ibis.expr.types as ir  # pyright: ignore [reportMissingTypeStubs]
+import polars as pl
+
+from polars_to_ibis._serialization import Serialization
+
+PolarsPlan = dict[str, Any]
+NamedValue = tuple[str, ir.Value]
+ReturnsTable = Callable[..., ir.Table]
+ReturnsValue = Callable[..., NamedValue]
+
+TABLE_REGISTRY: dict[str, ReturnsTable] = {}
+VALUE_REGISTRY: dict[str, ReturnsValue] = {}
+
+
+# Public interface:
+
+
+def polars_to_ibis(lf: pl.LazyFrame, table_name: str):
+    polars_plan = Serialization(lf)._serial  # type: ignore
+    polars_schema = lf.collect_schema()
+    ibis_schema = ibis.expr.schema.Schema.from_polars(polars_schema)
+    ibis_table = ibis.table(ibis_schema, name=table_name)  # type: ignore
+    new_table = polars_plan_to_ibis_table(
+        polars_plan=polars_plan,
+        table=ibis_table,
+    )
+    return new_table
+
+
+# Decorators:
+
+
+def table_handler(tag: str) -> Callable[..., ReturnsTable]:
+    def deco(func: ReturnsTable) -> ReturnsTable:
+        TABLE_REGISTRY[tag] = func
+        return func
+
+    return deco
+
+
+# def value_handler(tag: str) -> Callable[..., ReturnsValue]:
+#     def deco(func: ReturnsValue) -> ReturnsValue:
+#         VALUE_REGISTRY[tag] = func
+#         return func
+
+#     return deco
+
+
+# Helpers:
+
+
+def split_tag_payload(polars_plan: PolarsPlan) -> tuple[str, Any]:
+    if len(polars_plan) != 1:
+        raise ValueError(
+            f"Expected single-key tagged dict, got: {polars_plan!r}"
+        )  # pragma: no cover
+    return next(iter(polars_plan.items()))
+
+
+def polars_plan_to_ibis_table(polars_plan: PolarsPlan, table: ir.Table) -> ir.Table:
+    tag, payload = split_tag_payload(polars_plan)
+    try:
+        func = TABLE_REGISTRY[tag]
+    except KeyError as e:  # pragma: no cover
+        raise NotImplementedError(f"No table handler for {tag!r}") from e
+    return func(payload, table=table)
+
+
+# def polars_plan_to_ibis_value(polars_plan: PolarsPlan) -> NamedValue:
+#     tag, payload = split_tag_payload(polars_plan)
+#     try:
+#         func = VALUE_REGISTRY[tag]
+#     except KeyError as e:
+#         raise NotImplementedError(f"No value handler for {tag!r}") from e
+#     return func(payload)
+
+
+# def polars_plans_to_ibis_values(plans: list[PolarsPlan]) -> dict[str, ir.Value]:
+#     return dict(map(polars_plan_to_ibis_value, plans))
+
+
+# Handlers:
+
+
+@table_handler("Scan")
+@table_handler("DataFrameScan")
+def handle_source(payload: PolarsPlan, table: ir.Table) -> ir.Table:
+    return table
+
+
+# @table_handler("Select")
+# def handle_select(payload: PolarsPlan, table: ir.Table) -> ir.Table:
+#     input_table = polars_plan_to_ibis_table(payload["input"], table=table)
+#     ibis_values = polars_plans_to_ibis_values(payload.get("expr", []))
+#     return input_table.aggregate(**ibis_values)  # type: ignore
+
+
+@table_handler("MapFunction")
+def handle_map_function(payload: PolarsPlan, table: ir.Table) -> ir.Table:
+    input_table = polars_plan_to_ibis_table(payload["input"], table=table)
+    stats = payload["function"]["Stats"]
+    match stats:
+        case "Sum":
+            return table.aggregate(
+                [
+                    getattr(getattr(input_table, col), stats.lower())()
+                    for col in table.columns
+                ]
+            ).rename(lambda name: re.sub(r"^\w+\((.*)\)$", r"\1", name))
+
+        case _:  # pragma: no cover
+            raise ValueError(f"unsupported stats type: {stats}")
+
+
+# @value_handler("Agg")
+# def handle_agg(payload: Any) -> NamedValue:
+#     agg, agg_payload = split_tag_payload(payload)
+
+#     match agg:
+#         case "Sum":
+#             name, value = polars_plan_to_ibis_value(agg_payload)
+#             return name, value.sum()  # type: ignore
+
+#         case _:
+#             raise ValueError(f"unsupported agg type: {agg}")
---
+++ tests/test_parser.py
@@ -0,0 +1,106 @@
+from os import environ
+from typing import Any
+
+import ibis  # type: ignore
+import polars as pl
+import pytest
+
+from polars_to_ibis._parser import polars_to_ibis
+
+ibis.set_backend("polars")
+
+# Utilities:
+
+
+def get_connection(df: pl.DataFrame, table_name: str, backend: str):
+    kwargs = (
+        {
+            "user": environ["USER"],
+            "password": "",
+            "database": environ["USER"],
+        }
+        if backend == "mysql"
+        else {}
+    )
+    connection = getattr(ibis, backend).connect(**kwargs)
+
+    # Ensure a clean slate.
+    # Each backend raises its own error type
+    # if the table doesn't already exist.
+    try:
+        connection.drop_table(table_name)
+    except BaseException:  # noqa: B036
+        pass
+    connection.create_table(table_name, df)
+
+    return connection
+
+
+# Test fixtures:
+
+backends = [
+    # "polars",
+    "sqlite",
+    # "duckdb",
+    # pytest.param("postgres", marks=pytest.mark.extra_install),
+    # pytest.param("mysql", marks=pytest.mark.extra_install),
+]
+
+input_data = {
+    # "namespace": pl.DataFrame(
+    #     {
+    #         "ints": [1, 2, 3, 4],
+    #         # TODO: Add more columns, once polars namespace works on at least one
+    #     }
+    # ),
+    # "mixed": pl.DataFrame(
+    #     {
+    #         "ints": [1, 2, 3, 4],
+    #         "floats": [0.1, 0.2, 0.3, 0.4],
+    #         "strings": ["a", "b", "c", "d"],
+    #         "bools": [True, True, False, False],
+    #     }
+    # ),
+    "numeric": pl.DataFrame(
+        {
+            "ints": [1, 2, 3, 4],
+            "floats": [0.1, 0.2, 0.3, 0.4],
+        }
+    ),
+}
+
+category_expression_output_triples = [
+    ("numeric", "lf.sum()", {"floats": [1.0], "ints": [10]}),
+]
+
+
+# Tests:
+
+
+@pytest.mark.parametrize(
+    "category_expression_output",
+    category_expression_output_triples,
+    ids=lambda triple: "-".join(triple[:-1]),  # Don't include output in ID.
+)
+@pytest.mark.parametrize("backend", backends)
+def test_translate_table(
+    category_expression_output: tuple[str, str, dict[str, list[Any]]], backend: str
+):
+    # Setup:
+    category, expression, expected_output = category_expression_output
+    input_df = input_data[category]
+    lf = input_df.lazy()  # type: ignore # noqa: F841; "lf" is used in eval()
+    lf: pl.LazyFrame = eval(expression)
+    polars_output = lf.collect().to_dict(as_series=False)
+    assert (
+        polars_output == expected_output
+    ), "Typo in test? Polars does not produce expected output."
+
+    table_name = "default_table"
+    ibis_table = polars_to_ibis(lf, table_name)
+
+    connection = get_connection(input_df, table_name=table_name, backend=backend)
+    actual_output = connection.to_pandas(ibis_table).to_dict(orient="list")
+    assert (
+        actual_output == expected_output
+    ), f"Via ibis, {backend} does not produce expected output"
