1c61878d21ff8f7619de3bc2cca26663f47c6799
Chuck McCallum <mccallucc@gmail.com>
2026-06-15 15:05:25 -0400

    Serialization of quantile changed in latest Polars. Restore "convert_error" that was dropped in last merge.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -103,7 +103,11 @@ def test_translate_table_new(fixture: Fixture, backend: str, exporter_key: str):
     globals = {"lf": scan_database(connection, table_name), "pl": pl}
     lf = eval(fixture.expression, globals)
 
-    ibis_table = convert_polars_to_ibis(lf, table_name)
+    ibis_table = assert_error_or_none(
+        "convert_error",
+        fixture.convert_errors.get(f"polars=={pl.__version__}"),
+        lambda: convert_polars_to_ibis(lf, table_name),
+    )
 
     # Run query on target database:
     export = exporters[exporter_key]  # type: ignore
