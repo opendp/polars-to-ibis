e854043e77c1db0cd828b9ed2528a639dd2ed66d
Chuck McCallum <mccallucc@gmail.com>
2026-07-30 10:33:14 -0400

    propose helper function

--- tests/test_split.py
+++ tests/test_split.py
@@ -49,9 +49,9 @@ def test_split_lazyframe(fixture):
     # Pretend we're software that uses OpenDP as a dependency.
     # (If there is non-OpenDP boilerplate, move it into the package.)
     dp.enable_features("contrib", "honest-but-curious")
-    lf = scan_database(connection, table_name)
+    schema_lf = scan_database(connection, table_name)
     context = dp.Context.compositor(
-        data=lf,
+        data=schema_lf,
         privacy_unit=dp.unit_of(contributions=1),
         privacy_loss=dp.loss_of(epsilon=1.0),
         split_evenly_over=1,
@@ -64,62 +64,75 @@ def test_split_lazyframe(fixture):
         "dp": dp,
         "pl": pl,
     }
-    query_lf = eval(expression, globals).release().lazy()
+    query = eval(expression, globals)
 
-    ibis_table, plugin_parameters = split_polars_on_ffi(
-        query_lf,
-        table_name=table_name,
-    )
+    # TODO: Confirm that this is the interface we want.
+    def helper_function_to_add_to_opendp(query, table_name, connection):
+        query_lf = query.release().lazy()
 
-    actual_sql = norm_sql(ibis_table.to_sql())
-    assert actual_sql == expected_sql
+        ibis_table, plugin_parameters = split_polars_on_ffi(
+            query_lf,
+            table_name=table_name,
+            # TODO: pass a lib keyword, to make sure we're pulling out the right plugin,
+            # and since we're calling pickle, make sure that we're unpickling opendp
+            # ... though a filename match isn't really a security guarantee.
+        )
 
-    # TODO: Make a util in OpenDP so the user doesn't need to unpickle,
-    # and isn't tempted to look at private data.
+        actual_sql = norm_sql(ibis_table.to_sql())
+        assert actual_sql == expected_sql
 
-    plugin_parameters["lib"] = re.sub(r".*/", ".../", plugin_parameters["lib"])
+        plugin_parameters["lib"] = re.sub(r".*/", ".../", plugin_parameters["lib"])
 
-    # TODO: Probably replace with https://github.com/google/saferpickle
-    import pickle
+        # TODO: Probably replace with https://github.com/google/saferpickle
+        # ... but that is work that can be done in opendp, after porting.
+        import pickle
 
-    kwargs = pickle.loads(bytes(plugin_parameters["kwargs"]))
-    plugin_parameters["unpickled_kwargs"] = kwargs
-    del plugin_parameters["kwargs"]
+        kwargs = pickle.loads(bytes(plugin_parameters["kwargs"]))
+        plugin_parameters["unpickled_kwargs"] = kwargs
+        del plugin_parameters["kwargs"]
 
-    assert plugin_parameters == {
-        "flags": {"check_lengths": True, "flags": "ROW_SEPARABLE | LENGTH_PRESERVING"},
-        "lib": ".../opendp.abi3.so",
-        "symbol": "noise_plugin",
-        "unpickled_kwargs": {
-            "distribution": "Laplace",
-            "scale": 1.0,
-            "support": "Integer",
-        },
-    }
+        assert plugin_parameters == {
+            "flags": {
+                "check_lengths": True,
+                "flags": "ROW_SEPARABLE | LENGTH_PRESERVING",
+            },
+            "lib": ".../opendp.abi3.so",
+            "symbol": "noise_plugin",
+            "unpickled_kwargs": {
+                "distribution": "Laplace",
+                "scale": 1.0,
+                "support": "Integer",
+            },
+        }
 
-    private_result = connection.to_polars(ibis_table).to_dict(as_series=False)
-    assert private_result == expected_result
-    private_item = list(private_result.items())[0][1][0]
+        private_result = connection.to_polars(ibis_table).to_dict(as_series=False)
+        assert private_result == expected_result
+        private_item = list(private_result.items())[0][1][0]
 
-    # TODO: Confirm that we don't need any more information from the serialization.
-    match kwargs["support"]:
-        case "Integer":
-            support = int
-        case "Float":  # pragma: no cover
-            support = float
-        case _:  # pragma: no cover
-            raise ValueError(f"Expected 'Integer' or 'Float', not {kwargs['support']}")
-    input_space = dp.atom_domain(T=support, nan=False), dp.absolute_distance(T=support)
+        match kwargs["support"]:
+            case "Integer":
+                support = int
+            case "Float":  # pragma: no cover
+                support = float
+            case _:  # pragma: no cover
+                raise ValueError(
+                    f"Expected 'Integer' or 'Float', not {kwargs['support']}"
+                )
+        input_space = dp.atom_domain(T=support, nan=False), dp.absolute_distance(
+            T=support
+        )
 
-    match kwargs["distribution"]:
-        case "Laplace":
-            make = dp.m.make_laplace
-        case "Gaussian":  # pragma: no cover
-            make = dp.m.make_gaussian
-        case _:  # pragma: no cover
-            raise ValueError(
-                f"Expected 'Laplace' or 'Gaussian', not {kwargs['distribution']}"
-            )
-    measurement = make(*input_space, scale=kwargs["scale"])
+        match kwargs["distribution"]:
+            case "Laplace":
+                make = dp.m.make_laplace
+            case "Gaussian":  # pragma: no cover
+                make = dp.m.make_gaussian
+            case _:  # pragma: no cover
+                raise ValueError(
+                    f"Expected 'Laplace' or 'Gaussian', not {kwargs['distribution']}"
+                )
+        measurement = make(*input_space, scale=kwargs["scale"])
+        return measurement(private_item)
 
-    assert isinstance(measurement(private_item), support)
+    dp_result = helper_function_to_add_to_opendp(query, table_name, connection)
+    assert isinstance(dp_result, float) or isinstance(dp_result, int)
