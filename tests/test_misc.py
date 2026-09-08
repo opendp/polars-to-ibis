5b4c897d5b3fd97b45081e44081fb673e2afa45f
Chuck McCallum <mccalluc@users.noreply.github.com>
2026-09-08 14:02:23 -0400

    Move example from README to API docs (#127)
    - follow-up to #126: That was small, and to confirm that gh-pages was
    working, I self-merged.
    - This PR moves the example from the readme to the API docs. They each
    have their role, but I think usage info fits better in the API docs.
    - I think at some point we need better user-facing examples of what is
    covered, and what is not, but not in this PR. I've filed #128 for later.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -38,9 +38,9 @@ def test_polars_versions_in_ci_matrix():
     assert MAX_POLARS in tested_polars_versions
 
 
-def test_polars_versions_in_readme():
-    readme = (Path(__file__).parent.parent / "README.md").read_text()
-    assert all(v in readme for v in get_tested_polars_versions())
+def test_polars_versions_in_docs():
+    docs = (Path(__file__).parent.parent / "src/polars_to_ibis/__init__.py").read_text()
+    assert all(v in docs for v in get_tested_polars_versions())
 
 
 # @pytest.mark.parametrize(
