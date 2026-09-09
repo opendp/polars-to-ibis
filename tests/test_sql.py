efe828989bf9cd1010e5e9ee46ff0b59c765d22b
Chuck McCallum <mccallucc@gmail.com>
2026-09-09 12:04:44 -0400

    remove POC

--- tests/test_sql.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-
-def test_sql():
-    lf = pl.LazyFrame({"a": [1, 2, 3, 4], "b": [4, 5, 6, 7]})  # noqa: F841
-    query = pl.sql("SELECT SUM(a) FROM lf")
-    result = query.collect().to_dict(as_series=False)
-    assert result == {"a": [10]}
